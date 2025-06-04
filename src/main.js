63613f62748e2855feabf8d26c3de03a4d9539a2
Akram <32903348+akramram@users.noreply.github.com>
2025-06-04 09:26:11 +0700

    Adds Tailwind

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 // If you created src/assets/main.css, import it here:
-// import './assets/main.css'
+import './assets/main.css'
 
 const app = createApp(App)
 app.mount('#app') // This #app corresponds to the div in your public/index.html
